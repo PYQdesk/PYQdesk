abebb465dd0400ad1e23d8e86d8294e8cb4f0482
PYQdesk <prowayd.1290@gmail.com>
2025-03-14 15:54:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function redirectToYear(category) {
     window.location.href = `year.html?category=${category}`;
 }
+function redirectToSorry(category) {
+    window.location.href = `sorry.html?
+category=${category}`;
+}
